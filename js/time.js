10e9259a7a20355d6bb777f175505091b3602ce0
rfkdali <rfkdali@gmail.com>
2015-01-12 15:54:56 +0100

    add time-dependent feature : dynamic stylesheets

--- js/time.js
+++ js/time.js
@@ -15,3 +15,14 @@ function horloge() {
 }
 //execution
 horloge();
+
+//changement css en fonction de jour/nuit
+function styleSwitch () {
+	if(h>6 && h<19){
+		dayStyle = "css/morning.css";
+	}else{
+		dayStyle = "css/night.css";
+	}
+	document.getElementsByTagName('head')[0].innerHTML = '<link rel="stylesheet" type="text/css" href="css/styles.css">' + '<link rel="stylesheet" type="text/css" href="'+dayStyle+'">';
+}
+styleSwitch();
