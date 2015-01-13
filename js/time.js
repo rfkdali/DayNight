8ae26aebc7ee741639c65352aa10324d8ab8b8bd
rfkdali <rfkdali@gmail.com>
2015-01-13 13:52:11 +0100

    add progress bar for seconds, minutes and hours

--- js/time.js
+++ js/time.js
@@ -23,6 +23,43 @@ function styleSwitch () {
 	}else{
 		dayStyle = "css/night.css";
 	}
-	document.getElementsByTagName('head')[0].innerHTML = '<link rel="stylesheet" type="text/css" href="css/styles.css">' + '<link rel="stylesheet" type="text/css" href="'+dayStyle+'">';
+	document.getElementsByTagName('head')[0].innerHTML = '<link rel="stylesheet" type="text/css" href="css/styles.css">'+ '<link rel="stylesheet" type="text/css" href="css/jauge.css">' + '<link rel="stylesheet" type="text/css" href="'+dayStyle+'">';
 }
 styleSwitch();
+
+
+var i = 0;
+var j = 0;
+var timer;
+
+function jauge () {
+	console.log(i)
+	i++;
+	console.log("test : " + (i-1));
+	document.getElementById('jauge').style.width = s * 15 + 'px';
+	document.getElementById('position').style.left = s * 15 + 'px';
+	document.getElementById('position').innerHTML = s;
+
+	document.getElementById('jauge2').style.width = m * 15 + 'px';
+	document.getElementById('position2').style.left = m * 15 + 'px';
+	document.getElementById('position2').innerHTML = m;
+	console.log(m);
+
+	document.getElementById('jauge3').style.width = h * 15 + 'px';
+	document.getElementById('position3').style.left = h * 15 + 'px';
+	document.getElementById('position3').innerHTML = h;
+	console.log(h);
+	
+
+	timer = setTimeout(jauge, 1000)
+
+	if(j==24){
+		stopTimer()
+	}
+}
+
+function stopTimer () {
+	clearTimeout(timer)	
+}
+
+jauge();
