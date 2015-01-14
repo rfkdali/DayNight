e4e90a5344bbc843ab1565abd856e4548fa76bc9
rfkdali <rfkdali@gmail.com>
2015-01-14 18:44:19 +0100

    cleaning code episode II

--- js/time.js
+++ js/time.js
@@ -1,8 +1,11 @@
+
 function horloge() {
 	var date = new Date();
-	h = date.getHours();
-	m = date.getMinutes();
-	s = date.getSeconds();
+	var h = date.getHours();
+	var m = date.getMinutes();
+	var s = date.getSeconds();
+
+	var w = window.innerWidth;
 
 	if (s<10){
 			s = "" + 0 + s;
@@ -11,66 +14,69 @@ function horloge() {
 			m = "" + 0 + m;
 		}
 	document.getElementById("time").innerHTML = 'Il est ' + h + ":" + m + ":" + s;
+
+	toggleColor(s);
+	
+	pgBarWidth(w,s,m,h);
+	pgBarPosition(w,s,m,h);
+	pgBarData(w,s,m,h);
+
+	if(h > 6 && h < 19){
+		styleSwitch("css/morning.css");
+	} else {
+		styleSwitch("css/night.css");
+	}
+
 	setTimeout(horloge, 1000);
+
 }
 //execution
 horloge();
 
-//changement css en fonction de jour/nuit
-function styleSwitch () {
-	if(h>6 && h<19){
-		dayStyle = "css/morning.css";
-	}else{
-		dayStyle = "css/night.css";
+
+function styleSwitch (dayStyle) {
+	var styleSheetList = document.styleSheets;
+
+    var add_style = true
+	for(var i = 0; i < styleSheetList.length; i++) {
+		if (styleSheetList[i].href.indexOf(dayStyle) > 0) {
+			add_style = false
+		}
 	}
-	document.getElementsByTagName('head')[0].innerHTML = '<link rel="stylesheet" type="text/css" href="css/styles.css">'+ '<link rel="stylesheet" type="text/css" href="css/jauge.css">' + '<link rel="stylesheet" type="text/css" href="'+dayStyle+'">';
+	
+	if (add_style) {
+		var link = document.createElement('link')
+		link.setAttribute('rel', 'stylesheet')
+		link.setAttribute('type', 'text/css')
+		link.setAttribute('href', dayStyle)
+		document.getElementsByTagName('head')[0].appendChild(link)
+	}
 }
 
-styleSwitch();
 
-
-var i = 0;
-var j = 0;
-var timer;
-
-function pgBar () {
-
-	var w = window.innerWidth;
-	console.log('window width = ' + w);
-
-	function pgBarWidth () {
+function pgBarWidth (w,s,m,h) {
 		document.getElementById('jauge1').style.width = ((w * s)/60) + 'px';
 		document.getElementById('jauge2').style.width = ((w * m)/60) + 'px';
 		document.getElementById('jauge3').style.width = ((w * h)/24) + 'px';
 	}
-	pgBarWidth();
-
-	//let's go
-	
+function pgBarPosition (w,s,m,h) {
 	document.getElementById('position1').style.left = ((w * s)/60) + 'px';
-	document.getElementById('position1').innerHTML = s;
-	console.log('seconds : ' + s);
-
-	
 	document.getElementById('position2').style.left = ((w * m)/60) + 'px';
-	document.getElementById('position2').innerHTML = m;
-	console.log('minutes : ' + m);
-
-	
 	document.getElementById('position3').style.left = ((w * h)/24) + 'px';
+}
+function pgBarData (w,s,m,h) {
+	document.getElementById('position1').innerHTML = s;
+	document.getElementById('position2').innerHTML = m;
 	document.getElementById('position3').innerHTML = h;
-	console.log('hours : ' + h);
-	
+}
 
-	timer = setTimeout(pgBar, 1000);
+function toggleColor (s) {
+	var secondes = parseInt(s);
 
-	if(j==24){
-		stopTimer()
+	if((secondes % 2) == 0){
+		document.getElementById("time").className = "pair";
+	}
+	else{
+		document.getElementById("time").className = "impair";
 	}
 }
-
-function stopTimer () {
-	clearTimeout(timer)	
-}
-
-pgBar();
